d63e22664e3fde77ddcd459bd656224ffe509947
sesanaag <sesanaag@gmail.com>
2026-09-11 23:26:13 +0530

    chore: upgrade tinacms to 3.12+ and regenerate admin bundle

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types.js";
-export const client = createClient({ cacheDir: '/home/sesanaag/projects/heal_gurukuli/tina/__generated__/.cache/1788784348554', url: 'https://content.tinajs.io/2.4/content/e16c9dd9-dd86-4dc5-9116-67c8b1403b24/github/main', token: '35c4be521eeaee538727d9508e05ec165a622e34', queries,  });
+export const client = createClient({ cacheDir: '/home/sesanaag/projects/heal_gurukuli/tina/__generated__/.cache/1789149273836', url: 'https://content.tinajs.io/2.4/content/e16c9dd9-dd86-4dc5-9116-67c8b1403b24/github/main', token: '<your-token>', queries,  });
 export default client;
   
